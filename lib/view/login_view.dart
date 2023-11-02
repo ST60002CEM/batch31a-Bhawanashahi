8e469e0060ddb6fe8827439233ab1925d421787a
Bhawana shahi <shahivawana61@gmail.com>
2023-11-02 21:15:01 +0545

    remodified login page

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -2,26 +2,17 @@ import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
+  
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          const SizedBox(height: 50),
-          //logo
-          Icon(
-              Icons.lock,
-            size: 100,
-          )
-          //username textfield
-          //password textfield
-          //forgetpassword
-          //signin button
-          //register
-
-        ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white],
+          ),
+        ),
       ),
       
     );
