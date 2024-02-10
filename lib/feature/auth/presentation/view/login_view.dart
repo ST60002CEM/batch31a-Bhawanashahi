db525f862d9c0fb64aa08ad6ea5b8102dccb6a2d
Bhawana shahi <shahivawana61@gmail.com>
2024-02-10 22:53:43 +0545

    updated

--- lib/feature/auth/presentation/view/login_view.dart
+++ lib/feature/auth/presentation/view/login_view.dart
@@ -1,14 +1,12 @@
-// ignore_for_file: use_build_context_synchronously
-
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:online_pet_shop/config/router/app_route.dart';
 import 'package:online_pet_shop/core/common/snackbar/my_snackbar.dart';
 import 'package:online_pet_shop/feature/auth/presentation/auth_viewmodel/auth_viewmodel.dart';
 
-
 class LoginView extends ConsumerStatefulWidget {
-  const LoginView({super.key});
+  const LoginView({Key? key}) : super(key: key);
 
   @override
   ConsumerState<LoginView> createState() => _LoginViewState();
@@ -18,15 +16,12 @@ class _LoginViewState extends ConsumerState<LoginView> {
   final _formKey = GlobalKey<FormState>();
   final _usernameController = TextEditingController(text: 'zoro');
   final _passwordController = TextEditingController(text: 'zoro123');
-
-  // final _usernameController = TextEditingController();
-  // final _passwordController = TextEditingController();
-  final _gap = const SizedBox(height: 8);
   bool isObscure = true;
+
   @override
   Widget build(BuildContext context) {
     final authState = ref.watch(authViewModelProvider);
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
       if (authState.showMessage! && authState.error != null) {
         showSnackBar(message: 'Invalid Credentials', context: context);
         ref.read(authViewModelProvider.notifier).resetMessage();
@@ -42,7 +37,15 @@ class _LoginViewState extends ConsumerState<LoginView> {
                 padding: const EdgeInsets.all(8),
                 child: Column(
                   children: [
-                    _gap,
+                    // Add padding to move the image slightly above
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        width: 300, // Adjust width according to your needs
+                        height: 400, // Adjust height according to your needs
+                        child: Image.asset('assets/images/logo.png'),
+                      ),
+                    ),
                     TextFormField(
                       key: const ValueKey('username'),
                       controller: _usernameController,
@@ -57,7 +60,7 @@ class _LoginViewState extends ConsumerState<LoginView> {
                         return null;
                       },
                     ),
-                    _gap,
+                    SizedBox(height: 8),
                     TextFormField(
                       key: const ValueKey('password'),
                       controller: _passwordController,
@@ -82,20 +85,41 @@ class _LoginViewState extends ConsumerState<LoginView> {
                         return null;
                       }),
                     ),
-                    _gap,
+                    SizedBox(height: 8),
+                    // Aligning the Forgot Password text to the right
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: RichText(
+                        text: TextSpan(
+                          text: "Forgot Password?",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              // Implement your forgot password logic here
+                            },
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16), // Adding space between Forgot Password and Login Button
                     ElevatedButton(
                       onPressed: () async {
                         if (_formKey.currentState!.validate()) {
                           await ref
                               .read(authViewModelProvider.notifier)
                               .loginStudent(
-                                context,
-                                _usernameController.text,
-                                _passwordController.text,
-                              );
+                            context,
+                            _usernameController.text,
+                            _passwordController.text,
+                          );
                         }
                       },
-                      child: const SizedBox(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(Color(0xFFd8812F)),
+                      ),
+                      child: SizedBox(
                         height: 50,
                         child: Center(
                           child: Text(
@@ -108,23 +132,27 @@ class _LoginViewState extends ConsumerState<LoginView> {
                         ),
                       ),
                     ),
-                    const SizedBox(height: 8),
-                    ElevatedButton(
-                      key: const ValueKey('registerButton'),
-                      onPressed: () {
-                        Navigator.pushNamed(context, AppRoute.registerRoute);
-                      },
-                      child: const SizedBox(
-                        height: 50,
-                        child: Center(
-                          child: Text(
-                            'Register',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontFamily: 'Brand Bold',
-                            ),
-                          ),
+                    SizedBox(height: 8),
+                    RichText(
+                      text: TextSpan(
+                        text: "Don't have an account? ",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
                         ),
+                        children: [
+                          TextSpan(
+                            text: 'Sign up',
+                            style: TextStyle(
+                              color: Color(0xFFd8812F),
+                              fontWeight: FontWeight.bold,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.pushNamed(context, AppRoute.registerRoute);
+                              },
+                          ),
+                        ],
                       ),
                     ),
                   ],
